508dd7e982ba74236b6a2b8f337b4140dd21d7e6
root <root@ip-172-31-52-136.ec2.internal>
2019-11-01 08:30:25 +0000

    demo

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,17 +9,20 @@ data "aws_subnet_ids" "example" {
   vpc_id = "vpc-79ccb903"
 }
 
-data "aws_subnet" "example" {
-  count = "${length(data.aws_subnet_ids.example.ids)}"
-  id    = "${data.aws_subnet_ids.example.ids[count.index]}"
-}
-
+#data "aws_subnet" "example" {
+#  count = "${length(data.aws_subnet_ids.example.ids)}"
+#  id    = "${data.aws_subnet_ids.example.ids[count.index]}"
+#}
+#
 
 resource "aws_lb" "test-lb" {
   name = "test-lb-tf"
   internal = false
   load_balancer_type = "application"
-subnet_id = "${element(data.aws_subnet_ids.example.ids)}"
+  subnets = [
+	"subnet-c5e884fb",
+	"subnet-17eb035a",
+	]	
 }
 
 resource "aws_lb_target_group_attachment" "test" {
@@ -46,7 +49,7 @@ depends_on = [aws_instance.test-inst]
 resource "aws_instance" "test-inst" {
   instance_type = "${var.instance_type}"
   ami           = "${var.ami}"
-  count         = 1
+  count         = 2
   subnet_id     = "${var.subnet_id}"
   associate_public_ip_address = "${var.associate_public_ip_address}"
 
